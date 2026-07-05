f071be05f0339cf3c2a60e7f31d0a1f36a62d21f
SKIDGFX <katy@tsiserver.us>
2026-07-05 16:42:12 -0500

    Populated vector of Box named bricks with 5 bricks with even spacing. (Not Compile)

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,22 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
+
+	Box brick;
 	brick.width = 10;
 	brick.height = 2;
 	brick.x_position = 0;
 	brick.y_position = 5;
 	brick.doubleThick = true;
 	brick.color = ConsoleColor::DarkGreen;
+
+	bricks.push_back(brick);
+	int spacing = 5;
+	for (int i = 0; i < 4; i++)
+	{
+		brick.x_position += spacing + brick.width;
+		bricks.push_back(brick);
+	}
 }
 
 void Game::ResetBall()
